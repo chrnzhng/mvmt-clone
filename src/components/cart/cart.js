4a4546f9ba2c192818fab35a3046dc9bc6d1111d
Jordan Zhang <chrn.zhng@gmail.com>
2017-07-24 15:39:42 -0600

    Did some styling

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
+import { FormControl, FormGroup, ControlLabel, HelpBlock, Form, Checkbox, Radio, Button } from 'react-bootstrap';
+
 import './cart.css';
+
+function FieldGroup({ id, label, help, ...props }) {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
 
 class Cart extends Component {
     render() {
@@ -8,6 +20,109 @@ class Cart extends Component {
                 <section className="logo-header">
                     <img src={require("./logo-white.png")} />
                 </section>
+                <section className="cart-body">
+                <section className="left-form">
+                    <form>
+    <FieldGroup
+      id="formControlsText"
+      type="text"
+      label="Text"
+      placeholder="Enter text"
+    />
+    <FieldGroup
+      id="formControlsEmail"
+      type="email"
+      label="Email address"
+      placeholder="Enter email"
+    />
+    <FieldGroup
+      id="formControlsPassword"
+      label="Password"
+      type="password"
+    />
+    <FieldGroup
+      id="formControlsFile"
+      type="file"
+      label="File"
+      help="Example block-level help text here."
+    />
+
+    <Checkbox checked readOnly>
+      Checkbox
+    </Checkbox>
+    <Radio checked readOnly>
+      Radio
+    </Radio>
+
+    <FormGroup>
+      <Checkbox inline>
+        1
+      </Checkbox>
+      {' '}
+      <Checkbox inline>
+        2
+      </Checkbox>
+      {' '}
+      <Checkbox inline>
+        3
+      </Checkbox>
+    </FormGroup>
+    <FormGroup>
+      <Radio name="radioGroup" inline>
+        1
+      </Radio>
+      {' '}
+      <Radio name="radioGroup" inline>
+        2
+      </Radio>
+      {' '}
+      <Radio name="radioGroup" inline>
+        3
+      </Radio>
+    </FormGroup>
+
+    <FormGroup controlId="formControlsSelect">
+      <ControlLabel>Select</ControlLabel>
+      <FormControl componentClass="select" placeholder="select">
+        <option value="select">select</option>
+        <option value="other">...</option>
+      </FormControl>
+    </FormGroup>
+    <FormGroup controlId="formControlsSelectMultiple">
+      <ControlLabel>Multiple select</ControlLabel>
+      <FormControl componentClass="select" multiple>
+        <option value="select">select (multiple)</option>
+        <option value="other">...</option>
+      </FormControl>
+    </FormGroup>
+
+    <FormGroup controlId="formControlsTextarea">
+      <ControlLabel>Textarea</ControlLabel>
+      <FormControl componentClass="textarea" placeholder="textarea" />
+    </FormGroup>
+
+    <FormGroup>
+      <ControlLabel>Static text</ControlLabel>
+      <FormControl.Static>
+        email@example.com
+      </FormControl.Static>
+    </FormGroup>
+
+    <Button type="submit">
+      Submit
+    </Button>
+  </form>
+            </section>
+    <section className="cart-right">
+            YOUR CART:<br></br>
+            <div>
+            <img src={require('./sandstone-small.jpg')} />
+            Chrono Gunmetal $135.00<br></br>
+            Sandstone</div>
+            
+            
+    </section>
+            </section>
             </div>
         );
     }
