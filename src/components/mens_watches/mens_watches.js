1e640ca6b28fc1f54422e559396e6e3bd2b77968
Jordan Zhang <chrn.zhng@gmail.com>
2017-07-30 12:18:53 -0600

    Added new sections to producs view

--- src/components/mens_watches/mens_watches.js
+++ src/components/mens_watches/mens_watches.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Nav, Navbar, NavDropdown, MenuItem } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Nav, Navbar, NavDropdown, MenuItem} from 'react-bootstrap';
 import axios from 'axios';
 import WatchesProduct from './watches_products';
 import Header from '../header/header';
@@ -8,7 +8,7 @@ import Footer from '../footer/footer';
 import './mens_watches.css';
 
 class Watches extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             watches: []
@@ -16,83 +16,115 @@ class Watches extends Component {
 
     }
     componentDidMount() {
-        const results = axios.get(`http://localhost:3001/MensWatches`)
+        const results = axios
+            .get(`http://localhost:3001/MensWatches`)
             .then(res => res.data)
             .then((finalResult) => {
-                this.setState({
-                    watches: finalResult
-                });
+                this.setState({watches: finalResult});
                 console.log(this.state)
             });
     }
 
     render() {
 
-        const MensWatches = this.state.watches
+        const MensWatches = this
+            .state
+            .watches
             .map((watches, i) => {
                 return <WatchesProduct
-                            image = { watches.image }
-                            color = { watches.color }
-                            size = { watches.size }
-                            name = { watches.product_name }
-                            price = { watches.price }
-                            id = { watches.id }
-                            key = { watches.id }
-                    
-                            />                
-            }) 
+                    image={watches.image}
+                    color={watches.color}
+                    size={watches.size}
+                    name={watches.product_name}
+                    price={watches.price}
+                    id={watches.id}
+                    key={watches.id}/>
+            })
         return (
             <div>
-            <div className="header">
-                <Header />
-            </div>
-            <div className="banner-container-with-bg"> 
-                <img className="banner-img" src={require("./mens-chrono.jpg")}>   
-                </img>       
-            </div>
-            <section className="nav-bar-full">
-            <Navbar className="nav-styling">    
-                <Nav className="dropdown-coll">               
-                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1}>Action</MenuItem>
-                    <MenuItem eventKey={3.2}>Another action</MenuItem>
-                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey={3.4}>Separated link</MenuItem>
-                </NavDropdown>
-                </Nav>
-                <Nav className="dropdown-sort-filter">           
-                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1}>Action</MenuItem>
-                    <MenuItem eventKey={3.2}>Another action</MenuItem>
-                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey={3.4}>Separated link</MenuItem>
-                </NavDropdown>
-                {/* </Nav>
-                <Nav>                */}
-                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1}>Action</MenuItem>
-                    <MenuItem eventKey={3.2}>Another action</MenuItem>
-                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey={3.4}>Separated link</MenuItem>
-                </NavDropdown>
-                </Nav>
-            </Navbar>
-            </section>
-            <section className="watches-main-section">
-            <div className="watches-side-nav">
-            </div>          
-            
-              
-             <div className="watches-wrapper">
-               { MensWatches }
-            </div> 
-            </section>
-           
+                <div className="header">
+                    <Header/>
+                </div>
+                <div className="banner-container-with-bg">
+                    <img className="banner-img" src={require("./banner.jpeg")}></img>
+                </div>
+                <section className="nav-bar-full">
+                 
+                    <Navbar className="nav-styling">
+                        <div className="dropdown-coll">
+                        <Nav >
+                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                                <MenuItem eventKey={3.1}>Action</MenuItem>
+                                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                                <MenuItem divider/>
+                                <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                            </NavDropdown>
+                        </Nav>
+                        </div>
+                    
+                        <div className="dropdown-sort-filter">
+                        <Nav >
+                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                                <MenuItem eventKey={3.1}>Action</MenuItem>
+                                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                                <MenuItem divider/>
+                                <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                            </NavDropdown>
+               
+                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                                <MenuItem eventKey={3.1}>Action</MenuItem>
+                                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                                <MenuItem divider/>
+                                <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                            </NavDropdown>
+                        </Nav>
+                        </div>
+                    </Navbar>
+                </section>
+                <section className="watches-main-section">
+                    <div className="watches-side-nav"></div>
+
+                    <div className="watches-wrapper">
+                        {MensWatches}
+                    </div>
+
+                </section>
+                <section className="power-bottom">
+                    <div className="bottom-top-left">
+                        <img className="banner-img" src={require("./msunglass.jpeg")}></img>
+                    </div>
+                    <div className="bottom-top-right">
+                        <img className="banner-img" src={require("./mstrap.jpeg")}></img>
+                    </div>
+                </section>
+                <section className="explore-block">
+                    <div className="explore-top">
+                      <h2>EXPLORE MENS WATCHES</h2>
+                    </div>
+                    <div className="explore-bot">
+                       <p>From casual to formal your wrist is covered. Find the collection that<br></br> suits you.</p>
+                    </div>
+                </section>
+                <section className="four-watches">
+                    <div className="f-series">
+                        <img className="banner-img" src={require("./40.jpeg")}></img>
+                    </div>
+                    <div className="classic">
+                        <img className="banner-img" src={require("./classic.jpeg")}></img>
+                    </div>
+                    <div className="modern-sport">
+                        <img className="banner-img" src={require("./modern-sport.jpeg")}></img>
+                    </div>
+                    <div className="voyager">
+                        <img className="banner-img" src={require("./voyager.jpeg")}></img>
+                    </div>
+                </section>
+
                 <div className="footer">
-                    <Footer />
+                    <Footer/>
                 </div>
             </div>
         );
